0cf4b07a908cc2b346814379c2c12016725fe0a9
nikdsgn <nikdsgn@gmail.com>
2019-04-26 16:38:48 +0200

    Updates

---
+++ precache-manifest.18be303af93cf919a738c1f3041b87a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ad8868fd5fa8aec03db38c97b28a460",
+    "url": "/robofriendz/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriendz/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "1604f3c5924b05d9994a",
+    "url": "/robofriendz/static/css/main.6260b386.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriendz/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "1604f3c5924b05d9994a",
+    "url": "/robofriendz/static/js/main.7ce10320.chunk.js"
+  },
+  {
+    "revision": "33be00a70e23fc7560c3",
+    "url": "/robofriendz/static/js/runtime~main.77c9d905.js"
+  },
+  {
+    "revision": "884685928a8ae7cd4d53088a354afbd5",
+    "url": "/robofriendz/static/media/Stingray.88468592.woff"
+  }
+]);
